8f983b5ebb3761e13bf98bffcc8abde7726bab2f
sk-account <sonuk5031@gmail.com>
2021-11-25 08:19:33 +0530

    second commit

---
+++ src/test/java/com/flipcart/sampleTest.java
@@ -0,0 +1,20 @@
+package com.flipcart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class sampleTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
